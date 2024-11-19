7cff55f5971f9082d3a72aa35b0da2cc7a584f03
Josef Hedblom <hedblomjosef@gmail.com>
2024-11-19 14:31:44 +0100

    Update Category paths to work with Windows and Mac.

--- src/Category.java
+++ src/Category.java
@@ -1,15 +1,20 @@
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Category {
+    private static final String BASE_PATH = "src" + File.separator + "DB" + File.separator;
     private static final Map<String, String> CATEGORY_PATHS = new HashMap<>();
 
     static {
-        CATEGORY_PATHS.put("Brädspel", "src/DB/boardgame");
-        CATEGORY_PATHS.put("Film", "src/DB/film");
-        CATEGORY_PATHS.put("Musik", "src/DB/music");
-        CATEGORY_PATHS.put("TV", "src/DB/tv");
-        CATEGORY_PATHS.put("TV Spel", "src/DB/videogame");
+        addCategories("Brädspel", "Film", "Musik", "TV", "TV Spel");
+    }
+
+    private static void addCategories(String... categories) {
+        for (String category : categories) {
+            String subPath = category.toLowerCase().replace(" ", "");
+            CATEGORY_PATHS.put(category, BASE_PATH + subPath);
+        }
     }
 
     // public static getCategories hämta alla kategorier
